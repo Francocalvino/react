75b6e7afa7d60a4ccf140b1779724827a4e30d14
Fresh! <francocalvino89@gmail.com>
2021-08-12 19:58:46 +0200

    props y states

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,17 +1,47 @@
+import React from 'react';
 import './App.css';
 
 
-function Helloworld() {
+/* function Helloworld(props) {
   return (
-    <div id="hello">Hello World</div>
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+      </div>
   )
+} */
+
+class Helloworld extends React.Component {
+
+state= { 
+  show: true
+}
+
+  render(){
+    if(this.state.show){
+      return(
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.mytext}
+        <button>Cambiar estado</button>
+        </div>
+      )
+    } else {
+      return <h1>No hay elementos</h1>
+    }
+  }
 }
 
 
 
 function App() {
   return (
-    <div>This is my component: <Helloworld/> </div>
+      <div>
+        This is my component: 
+        <Helloworld mytext="Asique" subtitle="No"/>
+        <Helloworld mytext="Fresco" subtitle="Queda"/> 
+        <Helloworld mytext="metele" subtitle="Otra"/>
+      </div>
   );
 }
 
