ebbd5eab9fe9d95453468088dff65dd12c480485
Fresh! <francocalvino89@gmail.com>
2021-08-12 19:25:07 +0200

    primeros pasos

---
+++ myreact/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+function Helloworld() {
+  return (
+    <div id="hello">Hello World</div>
+  )
+}
+
+
+
+function App() {
+  return (
+    <div>This is my component: <Helloworld/> </div>
+  );
+}
+
+export default App;
