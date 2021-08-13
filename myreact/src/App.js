551c55d505abe8f3493d82afa32aed2a76954c20
Fresh! <francocalvino89@gmail.com>
2021-08-13 12:44:42 +0200

    .map props components

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,48 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+import tasks from './sample/task.json'
 
-/* function Helloworld(props) {
-  return (
-    <div id="hello">
-      <h3>{props.subtitle}</h3>
-      {props.mytext}
-      </div>
-  )
-} */
+import Tasks from './components/Tasks';
 
-class Helloworld extends React.Component {
 
-state= { 
-  show: true
-}
-
-  render(){
-    if(this.state.show){
-      return(
-        <div id="hello">
-        <h3>{this.props.subtitle}</h3>
-        {this.props.mytext}
-        <button>Cambiar estado</button>
-        </div>
-      )
-    } else {
-      return <h1>No hay elementos</h1>
-    }
+class App extends Component {
+  
+  state = {
+    tasks: tasks
   }
-}
 
 
-
-function App() {
-  return (
-      <div>
-        This is my component: 
-        <Helloworld mytext="Asique" subtitle="No"/>
-        <Helloworld mytext="Fresco" subtitle="Queda"/> 
-        <Helloworld mytext="metele" subtitle="Otra"/>
-      </div>
-  );
+  render() {
+    return <div>
+      {
+      <Tasks tasks={this.state.tasks}/>
+      }
+    </div>
+  }
 }
 
 export default App;
